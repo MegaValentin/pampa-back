eedcdc1f76dfa892e2842685cc690877b43289d7
unknown <valentimega@gmail.com>
2024-11-28 21:50:58 -0300

    fechaPublicacion

--- src/models/article.model.js
+++ src/models/article.model.js
@@ -21,10 +21,39 @@ const articleSchema = new mongoose.Schema({
         type: String,
         enum: ["borrador", "listo", "subido"], // Solo permite estos tres valores
         default: "borrador"
+    },
+    fechaPublicacion: {
+        type: Date,
+        default: null,
+        get: (date) => (date ? date.toISOString().split("T")[0] : null)
     }
 }, {
     toJSON: { getters: true },
     toObject: { getters: true }
 });
+
+articleSchema.pre("findOneAndUpdate", async function (next) {
+    const update = this.getUpdate(); 
+
+    if (update && update.status) {
+        switch (update.status) {
+            case "subido":
+                if (!this.get("fechaPublicacion")) {
+                    update.fechaPublicacion = new Date(); 
+                }
+                break;
+
+            case "borrador":
+                update.fechaPublicacion = null; 
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    next();
+});
+
 
 export default mongoose.model("Article", articleSchema);
